e78f777c417f114e8ee84a914cefe87b6540f6bb
Amir <amir_35@mail.ru>
2023-02-09 23:43:02 +0500

    5th example (if/else)

---
+++ examples/example5_ConditionsIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "амир")
+{
+    Console.WriteLine("Приветствую, милорд");
+}
+else
+{
+    Console.Write("Почему ты не на полях под палящим солнцем, ");
+    Console.WriteLine(username);
+}
